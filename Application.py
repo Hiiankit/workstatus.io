441519ec668879636b9dbeee6ac89794b0003ea0
Ankit singh <as1230641@gmail.com>
2024-09-30 19:05:13 +0530

    #

--- Application.py
+++ Application.py
@@ -32,10 +32,8 @@ class App:
         
     def submit_interval(self):
         self.capture_interval = self.capture_interval_var.get()  
-        print(f"Capture interval set to {self.capture_interval} minutes.")
+        print(f"Capture interval set to {self.capture_interval} seconds.")
         self.interval_entry.config(state="disabled")  # Disable the entry box
-        self.submit_button.config(state="disabled")  # Disable the submit button
-
 
         if self.capturing:
             self.capturing = False  # Stop the current loop
@@ -48,7 +46,7 @@ class App:
     def start_capturing(self):
         def capture_loop():
             while self.capturing:
-                self.screenshot_manager.capture_screenshot(blurred=False, submit_interval=self.capture_interval*60)
+                self.screenshot_manager.capture_screenshot(blurred=False, submit_interval=self.capture_interval)
         
         self.capture_thread = threading.Thread(target=capture_loop)
         self.capture_thread.daemon = True  # Make sure thread exits when the program ends
--- screenshot_manager.py
+++ screenshot_manager.py
@@ -1,32 +1,38 @@
 import pyautogui
 import cv2
 import os
-import boto3
+import time
 from dotenv import load_dotenv
 from datetime import datetime
 
 load_dotenv()
 
 class ScreenshotManager:
-    def __init__(self):
-        self.s3_client = boto3.client(
-            's3',
-            aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
-            aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY")
-        )
-        self.bucket_name = os.getenv("AWS_BUCKET_NAME")
+    def __init__(self , save_dir= None):
+        if save_dir is None:
+            # Path to Documents/workstatus_screenshots folder
+            self.save_dir = os.path.join(os.path.expanduser("~"), "Documents", "workstatus_screenshots")
+        else:
+            self.save_dir = save_dir
+    
+        if not os.path.exists(self.save_dir):
+            os.makedirs(self.save_dir)
+        
+        # self.delete_ss = DeleteScreenshots()
+        # self.delete_ss.delete_all_files()
 
-    def capture_screenshot(self, blurred=False):
-        screenshot = pyautogui.screenshot()
-        if blurred:
-            screenshot = screenshot.filter(cv2.GaussianBlur((15, 15)))  # Blurring the screenshot
-        timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
-        screenshot_file = f"screenshot_{timestamp}.png"
-        screenshot.save(screenshot_file)
-        self.upload_to_s3(screenshot_file)
-        os.remove(screenshot_file)  # Remove local file after upload
 
-    def upload_to_s3(self, filename):
-        self.s3_client.upload_file(filename, self.bucket_name, filename)
-        print(f"Uploaded {filename} to S3")
+    def capture_screenshot(self, blurred=False,submit_interval=300):
+        while True:
+            screenshot = pyautogui.screenshot()
+            if blurred:
+                screenshot = screenshot.filter(cv2.GaussianBlur((15, 15)))  # Blurring the screenshot
+            timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+            screenshot_file = os.path.join(self.save_dir, f"screenshot_{timestamp}.png")
+            screenshot.save(screenshot_file)
+            print("ss captured")
 
+            time.sleep(submit_interval)
+        
+        # self.upload_to_s3(screenshot_file)
+        # os.remove(screenshot_file)  # Remove local file after upload
