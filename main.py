f82dd0ef4005d0a487daed3df351582cde5db0c1
Ankit singh <as1230641@gmail.com>
2024-09-30 18:02:12 +0530

    chore: resolve merge conflict

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import schedule
-import time
+import time, os
 from activity_tracker import ActivityTracker
 from screenshot_manager import ScreenshotManager
+from handling_upload import HandlingUpload
+from Application import App
 
 def take_screenshot():
     screenshot_manager.capture_screenshot(blurred=False)
@@ -9,15 +11,27 @@ def take_screenshot():
 if __name__ == "__main__":
     activity_tracker = ActivityTracker()
     screenshot_manager = ScreenshotManager()
+    handling_up = HandlingUpload()
     
-    # Schedule screenshot every 10 minutes
-    schedule.every(1).minutes.do(take_screenshot)
+    # Schedule screenshot every 30 seconds
+    app = App(screenshot_manager, handling_up)  # Create an instance of App
+    schedule.every(30).seconds.do(take_screenshot)
 
-    # Start activity tracking in a separate thread
     import threading
     activity_thread = threading.Thread(target=activity_tracker.check_activity)
     activity_thread.start()
+    handling_up_thread = threading.Thread(target=handling_up.upload_to_s3)
+    handling_up_thread.start()
+    # screenshot_manager_thread = threading.Thread(target=screenshot_manager.capture_screenshot)
+    # screenshot_manager_thread.start()
 
+    app.run()
+        
+    handling_up.upload_all()
+
+    os._exit(0)
+    # Start activity tracking and upload handling in a separate thread
+    
     # Run scheduled tasks
     while True:
         schedule.run_pending()
